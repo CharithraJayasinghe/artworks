2da0965d195c8995f3309d6390d363b84c7ebce4
CharithraJayasinghe <charithraseneth3@gmail.com>
2024-08-09 10:00:16 +0530

    Initial Commit

---
+++ ArtWorks/ArtWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtWorksApp.swift
+//  ArtWorks
+//
+//  Created by Charithra Jayasingha on 2024-08-09.
+//
+
+import SwiftUI
+
+@main
+struct ArtWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtWorks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArtWorks
+//
+//  Created by Charithra Jayasingha on 2024-08-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
